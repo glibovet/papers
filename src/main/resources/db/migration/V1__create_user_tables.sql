7000c4988b04e521dfd97010152256053da5bd8d
borsch <oleh.kurpiak@gmail.com>
2016-12-19 11:01:34 +0200

    set step

--- src/main/resources/db/migration/V1__create_user_tables.sql
+++ src/main/resources/db/migration/V1__create_user_tables.sql
@@ -6,6 +6,8 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @@session.auto_increment_increment = 1;
+SET @@session.auto_increment_offset = 1;
 
 -- -----------------------------------------------------
 -- Schema papers
