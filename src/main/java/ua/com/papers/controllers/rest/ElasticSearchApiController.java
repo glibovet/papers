f1e440b91cc9d5f95f35984ea31d9d6d2addf709
borsch <oleh.kurpiak@gmail.com>
2017-04-22 19:42:51 +0300

    Refactoring

--- src/main/java/ua/com/papers/controllers/rest/ElasticSearchApiController.java
+++ src/main/java/ua/com/papers/controllers/rest/ElasticSearchApiController.java
@@ -10,15 +10,11 @@ import ua.com.papers.exceptions.service_error.ForbiddenException;
 import ua.com.papers.exceptions.service_error.ServiceErrorException;
 import ua.com.papers.exceptions.service_error.ValidationException;
 import ua.com.papers.pojo.dto.search.PublicationDTO;
-import ua.com.papers.pojo.entities.PublicationEntity;
 import ua.com.papers.pojo.response.Response;
 import ua.com.papers.pojo.response.ResponseFactory;
 import ua.com.papers.services.elastic.IElasticSearch;
-import ua.com.papers.services.search.ISearchService;
-import ua.com.papers.services.utils.SessionUtils;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Andrii on 12.11.2016.
@@ -33,53 +29,34 @@ public class ElasticSearchApiController {
     @Autowired
     private IElasticSearch elasticSearch;
 
-    @Autowired
-    private ISearchService searchService;
-
-    @RequestMapping(
-            value = "/index",
-            method = RequestMethod.POST
-    )
+    @RequestMapping(value = "/index", method = RequestMethod.POST)
     public @ResponseBody Response<Boolean> createIndex() throws ForbiddenException, ElasticSearchError {
         return responseFactory.get(elasticSearch.createIndexIfNotExist());
     }
 
-    @RequestMapping(
-            value = "/index_all",
-            method = RequestMethod.POST
-    )
+    @RequestMapping(value = "/index_all", method = RequestMethod.POST)
     public @ResponseBody Response<Boolean> indexAll() throws ForbiddenException, ElasticSearchError {
         return responseFactory.get(elasticSearch.indexAll());
     }
 
-    @RequestMapping(
-            value = "/index",
-            method = RequestMethod.DELETE
-    )
+    @RequestMapping(value = "/index", method = RequestMethod.DELETE)
     public @ResponseBody Response<Boolean> deleteIndex() throws PapersException {
         return responseFactory.get(elasticSearch.indexDelete());
     }
 
-    @RequestMapping(
-            value = "/publication/{id}/index",
-            method = RequestMethod.POST
-    )
-    public
-    @ResponseBody
-    Response<Boolean> indexPublication(
-            @PathVariable("id") int id) throws NoSuchEntityException, ForbiddenException, ServiceErrorException, ValidationException, ElasticSearchError {
+    @RequestMapping(value = "/publication/{id}/index", method = RequestMethod.POST)
+    public @ResponseBody Response<Boolean> indexPublication(
+            @PathVariable("id") int id
+    ) throws PapersException {
         return responseFactory.get(elasticSearch.indexPublication(id));
     }
 
-    @RequestMapping(
-            value = "/publication/search",
-            method = RequestMethod.GET
-    )
-    public
-    @ResponseBody
-    Response<List<PublicationDTO>> searchPublication(@RequestParam("q") String query,
-                                                        @RequestParam(value = "offset",  required = false, defaultValue = "0") int offset){
-        return responseFactory.get(searchService.search(query, offset));
+    @RequestMapping(value = "/publication/search", method = RequestMethod.GET)
+    public @ResponseBody Response<List<PublicationDTO>> searchPublication(
+            @RequestParam("q") String query,
+            @RequestParam(value = "offset",  required = false, defaultValue = "0") int offset
+    ){
+        return responseFactory.get(elasticSearch.search(query, offset));
     }
 
 }
