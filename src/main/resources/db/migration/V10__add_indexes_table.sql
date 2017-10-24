58a1c47803ec409e64bfc9968f40e1f4030be827
Xlopec <maksimolejn720@gmail.com>
2017-10-25 00:23:41 +0300

    sql syntax fix

--- src/main/resources/db/migration/V10__add_indexes_table.sql
+++ src/main/resources/db/migration/V10__add_indexes_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `papers`.`indexes` IF NOT EXISTS (`url` VARCHAR(320) NOT NULL PRIMARY KEY, `last_visit` TIMESTAMP, `content_hash` VARCHAR (32));
+CREATE TABLE IF NOT EXISTS `papers`.`indexes` (`url` VARCHAR(320) NOT NULL PRIMARY KEY, `last_visit` TIMESTAMP, `content_hash` VARCHAR (32));
